1110c5a018e028b2a06d46543bd85eb919b1dcaf
Sebastian Landwehr <info@dword-design.de>
2018-05-19 15:30:38 +0200

    jquery plugin

--- video-embed-privacy.js
+++ video-embed-privacy.js
@@ -1,11 +1,15 @@
+(function($) {
 
-jQuery(function() {
-	jQuery(".video-wrapped").each(function() {
-		jQuery(this)
+  $.fn.videoEmbedPrivacy = function() {
+    $(this)
 		.empty()
-		.append(jQuery('<div class="video-wrapped-play">').html(jQuery(this).attr('data-embed-play')))
+		.append($('<div class="video-wrapped-play">').html($(this).attr('data-embed-play')))
 		.click(function(e) { if (e.target.tagName.toLowerCase() !== 'a') {
-			jQuery(this).html(jQuery(this).attr('data-embed-frame').replace(/(\/embed\/[^"]*\\?[^"]*)/, '$1&autoplay=1')).addClass('video-wrapped-clicked')
+			$(this).html($(this).attr('data-embed-frame').replace(/(\/embed\/[^"]*\\?[^"]*)/, '$1&autoplay=1')).addClass('video-wrapped-clicked')
 		}} )
-	})
-});
+    return this;
+  };
+
+  $('.video-wrapped').videoEmbedPrivacy();
+
+}(jQuery));
